0bbbd7edfd689415b507646a2097a789a4e6a2c4
Vladyslav <vladislav.levoshko@gmail.com>
2026-02-27 19:25:36 +0100

    change prisma.ts, delete adapter sqlite

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from '../app/generated/prisma'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL,
-})
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
