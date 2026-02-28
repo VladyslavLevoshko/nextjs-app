875bbbb9c1c15945d1c579d40d47dfaaae0dfaf8
Vladyslav <vladislav.levoshko@gmail.com>
2026-02-28 14:30:13 +0100

    try with any, sixth try

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -47,7 +47,7 @@ export default async function PostsPage({ searchParams }: Props) {
         </header>
 
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {posts.map((p) => (
+          {posts.map((p: any) => (
             <PostCard
               key={p.id}
               id={p.id}
