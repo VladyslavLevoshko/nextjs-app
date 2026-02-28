ae997cbfdedface5abadeb0b02668599a400bc78
Vladyslav <vladislav.levoshko@gmail.com>
2026-02-28 15:01:49 +0100

    eleven try, add any to users

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -45,7 +45,7 @@ export default async function UserPage({ params }: { params: Promise<{ id: strin
           </div>
         ) : (
           <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {posts.map((p) => (
+            {posts.map((p: any) => (
               <PostCard
                 key={p.id}
                 id={p.id}
